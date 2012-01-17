34848ac43e614971b03cf96f915da46dc23f7fef
Petr Pilař <email@palmik.net>
2012-01-17 21:11:10 +0100

    What was in MongoDB.Functions is now in MongoDB.Funcstions.S (you can still use the old module for now). In MongoDB.Functions.M contains functions that let you select the database (you pass it the MongoDB Snaplet lens) on which you want the action to be run.

--- src/Snap/Snaplet/MongoDB/Core.hs
+++ src/Snap/Snaplet/MongoDB/Core.hs
@@ -1,10 +1,20 @@
 module Snap.Snaplet.MongoDB.Core
 ( MongoDB(..)
 , HasMongoDB(..)
+, mongoDBInit
 ) where
 
-import            Database.MongoDB
-import            System.IO.Pool
+import           Data.Text (Text)
+
+import           Snap
+
+import           Database.MongoDB
+import           System.IO.Pool
+
+------------------------------------------------------------------------------
+-- | Description text used in mongoDBInit as makeSnaplet argument.
+description :: Text
+description = "Minimalistic MongoDB Snaplet."
 
 ------------------------------------------------------------------------------
 -- | Snaplet's data type.
@@ -20,7 +30,7 @@ data MongoDB = MongoDB
     { mongoPool     :: Pool IOError Pipe
     , mongoDatabase :: Database
     }
-
+    
 ------------------------------------------------------------------------------
 -- | Snaplet's type-class.
 --
@@ -33,4 +43,23 @@ data MongoDB = MongoDB
 -- Note: The (.) is from Control.Category.
 class HasMongoDB app where
     getMongoDB :: app -> MongoDB
+
+------------------------------------------------------------------------------
+-- | Initializer function.
+-- 1. argument: Maximum pool size.
+-- 2. argument: Host (e.g. return value of MongoDB's host function).
+-- 3. argument: Database name.
+--
+-- Example:
+-- @
+-- app :: SnapletInit App App
+-- app = makeSnaplet "app" "An snaplet example application." Nothing $ do
+--     h <- nestSnaplet "heist" heist $ heistInit "resources/templates"
+--     d <- nestSnaplet "database" database $ mongoDBInit 10 (host "127.0.0.1") "Snaplet-MongoDB"
+--     return $ App h d
+-- @
+mongoDBInit :: Int -> Host -> Database -> SnapletInit app MongoDB
+mongoDBInit n h d = makeSnaplet "snaplet-mongodb" description Nothing $ do
+    pool <- liftIO $ newPool (Factory (connect h) close isClosed) n
+    return $ MongoDB pool d
     
